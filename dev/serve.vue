0efc79092b24c183f14347f19e4a0033b17d4531
Gonzalo Cavazos <gonzalo.adriancr@gmail.com>
2022-05-23 22:43:19 -0500

    Serve component example

--- dev/serve.vue
+++ dev/serve.vue
@@ -5,20 +5,73 @@ import Vue from 'vue';
 
 export default Vue.extend({
   name: 'ServeDev',
-  // components: {
-  //  VueCircularNavigatorSample,
-  // }
+  data(){
+    return {
+      wifi: false,
+      ecologic: true,
+      color: '',
+      conf: {    
+        /*top: '55vh',*/
+        top: '70vh',
+      },
+      options: [
+            {
+                id:1,
+                title: 'Wifi',
+                icon: 'bx bx-wifi',                
+                type:'switch',                
+                value: false,
+                disabled: false,
+            }, 
+            {
+                id:2,
+                title: 'Select color',
+                icon: 'bx bx-color',                
+                type:'action',
+                value: false,
+                disabled: false,
+            }, 
+            {
+                id:3,
+                title: 'Ecologic Mode',
+                icon: 'bx bx-leaf',                
+                type:'switch',
+                value: true,
+                disabled: false,
+            }, 
+        ]
+
+    }
+  },
+  methods:{
+    clicked(option){
+      if(option.id == 1){
+        this.wifi = option.value
+      }
+      if(option.id == 2){
+        this.color = prompt('Enter the color:')
+      }
+
+      if(option.id == 3){
+        this.ecologic = option.value;
+      }
+    }
+  }
 });
 </script>
 
 <template>
-  <div id="app" style="background-color: red">
-    <vue-circular-navigator />
+  <div id="app" >    
+    <strong>Wifi:</strong> {{ this.wifi ? 'On' : 'Off' }} <br>
+    <strong>Color:</strong> {{ this.color  }} <br>
+    <strong>Ecologic</strong> {{ this.ecologic ? 'On' : 'Off' }}
+    <vue-circular-navigator @optionClicked="clicked" :conf="conf" :options="options"/>
   </div>
 </template>
 
 <style >
-body{
-  background: purple;
+body{ 
+  background: #E0E0E0;
+  height: 1000px;
 }
 </style>
---
+++ src/lib-components/default.option.js
@@ -0,0 +1,8 @@
+export default {
+    title: '',
+    type: 'switch' /* 'switch|action' */ ,
+    iconDefaultColor: '#0e2431',
+    iconActiveColor: '#5f8e12',
+    value: false,
+    disabled: false,
+}
